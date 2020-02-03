7a5bed59807d731cbc67d964982293b749c5e8a0
guoang97 <384019118@qq.com>
2020-02-03 14:32:32 +0800

    webadmin

--- server/routers/webadmin/service.js
+++ server/routers/webadmin/service.js
@@ -1,14 +1,56 @@
 const db = require('../../plugins/db.js')
+let data
+const s={
+    "type":'SUCCESS',
+    "msg":"操作成功"
+}
+const e={
+    "type":'ERROE',
+     "msg":"操作失败"
+}
+const ep={
+    "type":'ERROR_PARAMS_EXIST',
+     "msg":"用户名重复"
+}
 exports.registered = (req,res) => {
     let info = {
         username:req.body.username,
         password:req.body.password
     }
     let sql = 'insert into user set ?'
-    db.base(sql,info,(result) =>{
-        let data={
-            type:'SUCCESS'
+    db.base(sql,info,(result,error) =>{
+        if(error&&error.errno==1062){
+            data={
+                state:ep,
+                data:{}
+                }
+        }else{
+            data={
+                state:s,
+                data:{}
+                }
         }
+        
         res.send(data)
+    })
+}
+
+exports.login = (req,res) => {
+    let info = [req.body.username,req.body.password]
+    let sql = 'select * from user where username=? and password=?'
+    db.base(sql,info,(result) =>{
+        if(result.length==0){    
+            data={
+                state:e,
+                data:{}
+                }   //    数据库里面没找到配对的内容返回参数
+        }else{
+         data={
+            state:s,
+            data:{}
+            }//返回登录成功
+        }
+        console.log(result)
+        res.send(data); 
     })
 }
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import index from '../views/index.vue'
+import home from "../views/home.vue"
+import adminhome from "../views/webadmin/adminhome.vue"
 
 Vue.use(VueRouter)
 
@@ -8,7 +10,30 @@ const routes = [
   {
     path: '/',
     name: 'index',
-    component: index
+    redirect:'/home',
+    component: index,
+  },
+  {
+    path: '/index',
+    name: 'index',
+    component: index,
+    redirect:'/home',
+    children: [
+      {
+        path: '*',
+        redirect:'/home',
+        },
+      {
+        path: '/home',
+        name: 'home',
+        component: home  
+      } ,
+      {
+        path: '/adminhome',
+        name: 'adminhome',
+        component: adminhome  
+      } 
+    ]
   },
 ]
 
